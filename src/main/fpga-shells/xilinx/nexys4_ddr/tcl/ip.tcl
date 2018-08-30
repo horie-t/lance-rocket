671bc0bb62a611ca1a8eee2ab5dcd752662e49d0
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2018-08-30 12:19:22 +0900

    VRAMのIPを削除

--- src/main/fpga-shells/xilinx/nexys4_ddr/tcl/ip.tcl
+++ src/main/fpga-shells/xilinx/nexys4_ddr/tcl/ip.tcl
@@ -30,22 +30,3 @@ set_property -dict [list \
 	CONFIG.C_PROBE14_TYPE {1} \
 	CONFIG.C_PROBE14_WIDTH {97} \
 	] [get_ips ila]
-
-# VRAM用ブロックメモリ
-create_ip -name blk_mem_gen -vendor xilinx.com -library ip -module_name Vram -dir $ipdir -force
-set_property -dict [list \
-	CONFIG.Memory_Type {True_Dual_Port_RAM} \
-	CONFIG.Use_Byte_Write_Enable {true} \
-	CONFIG.Byte_Size {8} \
-	CONFIG.Write_Width_A {32} \
-	CONFIG.Write_Depth_A {131072} \
-	CONFIG.Read_Width_A {32} \
-	CONFIG.Write_Width_B {8} \
-	CONFIG.Read_Width_B {8} \
-	CONFIG.Enable_B {Use_ENB_Pin} \
-	CONFIG.Register_PortA_Output_of_Memory_Primitives {false} \
-	CONFIG.Register_PortB_Output_of_Memory_Primitives {false} \
-	CONFIG.Port_B_Clock {100} \
-	CONFIG.Port_B_Write_Rate {50} \
-	CONFIG.Port_B_Enable_Rate {100} \
-	] [get_ips Vram]
