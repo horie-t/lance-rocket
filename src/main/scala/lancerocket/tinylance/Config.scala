19c191bdf084b74dbd72dff3821c3a6ed5a897c8
HORIE Tetsuya <tetsuya.ettem@gmail.com>
2018-09-01 14:19:05 +0900

    TinyLanceのデータのスクラッチパッドメモリを増加

--- src/main/scala/lancerocket/tinylance/Config.scala
+++ src/main/scala/lancerocket/tinylance/Config.scala
@@ -7,6 +7,7 @@ import freechips.rocketchip.subsystem._
 import freechips.rocketchip.devices.debug._
 import freechips.rocketchip.devices.tilelink._
 import freechips.rocketchip.diplomacy.{DTSModel, DTSTimebase}
+import freechips.rocketchip.rocket._
 import freechips.rocketchip.system._
 import freechips.rocketchip.tile._
 
@@ -15,12 +16,43 @@ import sifive.blocks.devices.gpio._
 import sifive.blocks.devices.uart._
 import sifive.blocks.devices.seg7._
 
+class With1TinyLanceCore extends Config((site, here, up) => {
+  case XLen => 32
+  case RocketTilesKey => List(RocketTileParams(
+      core = RocketCoreParams(
+        useVM = false,
+        fpu = None,
+        mulDiv = Some(MulDivParams(mulUnroll = 8))),
+      btb = None,
+      dcache = Some(DCacheParams(
+        rowBits = site(SystemBusKey).beatBits,
+        nSets = 512, // 32Kb scratchpad
+        nWays = 1,
+        nTLBEntries = 4,
+        nMSHRs = 0,
+        blockBytes = site(CacheBlockBytes),
+        scratch = Some(0x80000000L))),
+      icache = Some(ICacheParams(
+        rowBits = site(SystemBusKey).beatBits,
+        nSets = 64,
+        nWays = 1,
+        nTLBEntries = 4,
+        blockBytes = site(CacheBlockBytes)))))
+  case RocketCrossingKey => List(RocketCrossingParams(
+    crossingType = SynchronousCrossing(),
+    master = TileMasterPortParams()
+  ))
+})
+
 // Default TinyLanceConfig
 class DefaultTinyLanceConfig extends Config (
   new WithNBreakpoints(2)        ++
   new WithNExtTopInterrupts(0)   ++
   new WithJtagDTM                ++
-  new TinyConfig
+  new WithNoMemPort ++
+  new WithNMemoryChannels(0) ++
+  new With1TinyLanceCore ++
+  new BaseConfig
 )
 
 // TinyLance Peripherals
@@ -28,7 +60,7 @@ class TinyLancePeripherals extends Config((site, here, up) => {
   case PeripheryGPIOKey => List(
     GPIOParams(address = 0x10012000, width = 32, includeIOF = true))
   case PeripheryUARTKey => List(
-    UARTParams(address = 0x10013000))
+    UARTParams(address = 0x10013000, nTxEntries = 256, nRxEntries = 256))
   case PeripherySeg7LEDKey => List(
     Seg7LEDParams(address = 0x10017000))
   case PeripheryMockAONKey =>
